38e7004d024e417843c3a80131d449cd5ed6143c
Edroamz <35645733+edroamz@users.noreply.github.com>
2022-12-28 17:16:54 -0600

    add tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import "./main.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="text-3xl text-blue-800">Testing tailwindcss</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
