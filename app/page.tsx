c94a38b39103b67c1ecb1986c4327c6bcd7c43cd
Edroamz <35645733+edroamz@users.noreply.github.com>
2023-09-09 07:47:28 -0600

    initial config. Moved to pnpm. Manually installing nextjs, typescript, eslint...

---
+++ app/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+  return <h1 className="text-4xl font-black">Hello, Next.js!</h1>;
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,jsx}"],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
-    extend: {
-      keyframes: {
-        "accordion-down": {
-          from: { height: 0 },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: 0 },
-        },
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-      },
-    },
+    extend: {},
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [],
 };
