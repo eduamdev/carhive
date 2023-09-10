b34186f8586f92131bc2a09fbbf1f28dd3b97f1f
Edroamz <35645733+edroamz@users.noreply.github.com>
2023-09-10 08:22:50 -0600

    chore: add footer

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -1,8 +1,42 @@
 export function SiteFooter() {
   return (
     <footer className="bg-neutral-50">
-      <div className="mx-auto my-16 w-full max-w-none px-5 sm:max-w-[90%] sm:px-0 xl:max-w-6xl">
-        <p className="text-center">footer</p>
+      <div className="xl:max-w-8xl mx-auto mt-12 w-full max-w-none px-5 text-sm sm:max-w-[90%] sm:px-0">
+        <div className="grid grid-cols-3 items-center justify-between">
+          <ul className="flex flex-col gap-2.5">
+            <li className="font-medium">Company</li>
+            <li className="text-neutral-600">About</li>
+            <li className="text-neutral-600">Careers</li>
+            <li className="text-neutral-600">Mobile</li>
+            <li className="text-neutral-600">Blog</li>
+            <li className="text-neutral-600">How we work</li>
+            <li className="text-neutral-600">Log in</li>
+          </ul>
+          <ul className="flex flex-col gap-2.5">
+            <li className="font-medium">Contact</li>
+            <li className="text-neutral-600">About</li>
+            <li className="text-neutral-600">Careers</li>
+            <li className="text-neutral-600">Mobile</li>
+            <li className="text-neutral-600">Blog</li>
+            <li className="text-neutral-600">How we work</li>
+            <li className="text-neutral-600">Log in</li>
+          </ul>
+          <ul className="flex flex-col gap-2.5">
+            <li className="font-medium">More</li>
+            <li className="text-neutral-600">About</li>
+            <li className="text-neutral-600">Careers</li>
+            <li className="text-neutral-600">Mobile</li>
+            <li className="text-neutral-600">Blog</li>
+            <li className="text-neutral-600">How we work</li>
+            <li className="text-neutral-600">Log in</li>
+          </ul>
+        </div>
+        <div className="mt-12 border-t py-8">
+          <p className="text-neutral-600">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia,
+            ullam?
+          </p>
+        </div>
       </div>
     </footer>
   );
--- components/site-header.tsx
+++ components/site-header.tsx
@@ -3,11 +3,11 @@ import { Icons } from '@/components/icons';
 export function SiteHeader() {
   return (
     <header className="border-b border-black/10">
-      <div className="mx-auto h-16 w-full max-w-none px-5 sm:max-w-[90%] sm:px-0 xl:max-w-7xl">
+      <div className="xl:max-w-8xl mx-auto h-16 w-full max-w-none px-5 sm:max-w-[90%] sm:px-0">
         <div className="flex h-full items-center justify-between">
           <p className="font-bold">Car Dealer</p>
           <nav>
-            <Icons.user className="h-6 w-6" />
+            <Icons.user className="h-7 w-7" />
           </nav>
         </div>
       </div>
