f700ff073631546ccf5afe3108cbbafe5cdb489f
Edroamz <35645733+edroamz@users.noreply.github.com>
2023-09-19 15:07:55 -0600

    chore: add scroll-area component

--- components/popular-car-types.tsx
+++ components/popular-car-types.tsx
@@ -1,72 +1,78 @@
 import Image from 'next/image';
+import { ScrollArea, ScrollBar } from '@/components/ui/scroll-area';
 
 export function PopularCarTypes() {
   return (
     <section className="border-t pt-10">
       <div className="2xl:max-w-8xl mx-auto w-full max-w-none px-5 sm:max-w-[90%] sm:px-0">
         <h2 className="text-2xl font-extrabold">Popular Rental Car Choices</h2>
-        <div className="mt-8 inline-flex items-center justify-between gap-4 overflow-hidden whitespace-nowrap">
-          <div className="h-40 w-[262px] transition-all hover:grayscale">
-            <Image
-              width={262}
-              height={160}
-              priority
-              src="/suv.jpg"
-              alt="suv"
-              className="h-full w-full rounded-2xl border bg-gradient-to-r from-[#f9f9f9] to-[#e9e9e9] object-cover object-center"
-            />
+      </div>
+      <div className="relative mt-8 w-full before:absolute before:left-0 before:top-0 before:z-10 before:h-full before:w-[5%] before:bg-gradient-to-r before:from-white before:content-[''] after:absolute after:right-0 after:top-0 after:z-10 after:h-full after:w-[5%] after:bg-gradient-to-l after:from-white after:content-['']">
+        <ScrollArea>
+          <div className="mb-3 flex gap-x-3 whitespace-nowrap px-0">
+            <div className="ml-[calc(100vw*.05)] inline-block h-36 w-[250px] transition-all hover:grayscale">
+              <Image
+                width={250}
+                height={144}
+                priority
+                src="/suv.jpg"
+                alt="suv"
+                className="h-full w-full rounded-2xl border bg-gradient-to-r from-[#f9f9f9] to-[#e9e9e9] object-cover object-center"
+              />
+            </div>
+            <div className="inline-block h-36 w-[250px] transition-all hover:grayscale">
+              <Image
+                width={250}
+                height={144}
+                priority
+                src="/minivan.jpg"
+                alt="minivan"
+                className="h-full w-full rounded-2xl border bg-gradient-to-r from-[#f9f9f9] to-[#e9e9e9] object-cover object-center"
+              />
+            </div>
+            <div className="inline-block h-36 w-[250px] transition-all hover:grayscale">
+              <Image
+                width={250}
+                height={144}
+                priority
+                src="/pick-up.jpg"
+                alt="pick-up"
+                className="h-full w-full rounded-2xl border bg-gradient-to-r from-[#f9f9f9] to-[#e9e9e9] object-cover object-center"
+              />
+            </div>
+            <div className="inline-block h-36 w-[250px] transition-all hover:grayscale">
+              <Image
+                width={250}
+                height={144}
+                priority
+                src="/sedan.jpg"
+                alt="sedan"
+                className="h-full w-full rounded-2xl border bg-gradient-to-r from-[#f9f9f9] to-[#e9e9e9] object-cover object-center"
+              />
+            </div>
+            <div className="inline-block h-36 w-[250px] transition-all hover:grayscale">
+              <Image
+                width={250}
+                height={144}
+                priority
+                src="/convertible.jpg"
+                alt="convertible"
+                className="h-full w-full rounded-2xl border bg-gradient-to-r from-[#f9f9f9] to-[#e9e9e9] object-cover object-center"
+              />
+            </div>
+            <div className="mr-[calc(100vw*.05)] inline-block h-36 w-[250px] transition-all hover:grayscale">
+              <Image
+                width={250}
+                height={144}
+                priority
+                src="/off-road.jpg"
+                alt="off-road"
+                className="h-full w-full rounded-2xl border bg-gradient-to-r from-[#f9f9f9] to-[#e9e9e9] object-cover object-center"
+              />
+            </div>
+            <ScrollBar orientation="horizontal" />
           </div>
-          <div className="h-40 w-[262px] transition-all hover:grayscale">
-            <Image
-              width={262}
-              height={160}
-              priority
-              src="/minivan.jpg"
-              alt="minivan"
-              className="h-full w-full rounded-2xl border bg-gradient-to-r from-[#f9f9f9] to-[#e9e9e9] object-cover object-center"
-            />
-          </div>
-          <div className="h-40 w-[262px] transition-all hover:grayscale">
-            <Image
-              width={262}
-              height={160}
-              priority
-              src="/pick-up.jpg"
-              alt="pick-up"
-              className="h-full w-full rounded-2xl border bg-gradient-to-r from-[#f9f9f9] to-[#e9e9e9] object-cover object-center"
-            />
-          </div>
-          <div className="h-40 w-[262px] transition-all hover:grayscale">
-            <Image
-              width={262}
-              height={160}
-              priority
-              src="/sedan.jpg"
-              alt="sedan"
-              className="h-full w-full rounded-2xl border bg-gradient-to-r from-[#f9f9f9] to-[#e9e9e9] object-cover object-center"
-            />
-          </div>
-          <div className="h-40 w-[262px] transition-all hover:grayscale">
-            <Image
-              width={262}
-              height={160}
-              priority
-              src="/convertible.jpg"
-              alt="convertible"
-              className="h-full w-full rounded-2xl border bg-gradient-to-r from-[#f9f9f9] to-[#e9e9e9] object-cover object-center"
-            />
-          </div>
-          <div className="h-40 w-[262px] transition-all hover:grayscale">
-            <Image
-              width={262}
-              height={160}
-              priority
-              src="/off-road.jpg"
-              alt="off-road"
-              className="h-full w-full rounded-2xl border bg-gradient-to-r from-[#f9f9f9] to-[#e9e9e9] object-cover object-center"
-            />
-          </div>
-        </div>
+        </ScrollArea>
       </div>
     </section>
   );
