68fc4e3987a520db15e4d6bb105f64fc5ba19fd2
Edroamz <35645733+edroamz@users.noreply.github.com>
2023-10-18 16:04:16 -0600

    chores: delete previous data

--- src/data/navItems.js
+++
@@ -1,5 +0,0 @@
-export const navItems = [
-  { title: "Fleet", href: "/vehicles" },
-  { title: "Features", href: "/#features", isHashLink: true },
-  { title: "FAQs", href: "/#faqs", isHashLink: true },
-];
--- src/routes/index.jsx
+++
@@ -1,7 +0,0 @@
-import { Root } from "./Root";
-import { Home } from "./Home";
-import { VehicleGrid } from "./VehicleGrid";
-import { VehicleView } from "./VehicleView";
-import { NotFound } from "./NotFound";
-
-export { Root, Home, VehicleGrid, VehicleView, NotFound };
