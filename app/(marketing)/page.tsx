ef8b3cfdb07eea5cd7b5a027ad1e5c95c4f11c11
Edroamz <35645733+edroamz@users.noreply.github.com>
2023-12-01 14:03:18 -0600

    chore: remove bg-gradient

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -60,7 +60,7 @@ async function Hero() {
   const locations = await fetchLocations();
 
   return (
-    <section className="bg-gradient-to-b from-white via-neutral-50 to-neutral-50 pt-12">
+    <section className="bg-neutral-50 pt-12">
       <h1 className="text-center text-3xl font-extrabold">Find your car</h1>
       <div className="mx-auto mt-6 grid max-w-4xl grid-cols-1 items-center justify-center gap-4 md:flex md:flex-row md:gap-12">
         <div className="flex items-center justify-center gap-1.5">
