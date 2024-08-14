6cf9578889800f0d8e411eabec6e1982d6a642c1
eduamdev <35645733+eduamdev@users.noreply.github.com>
2024-08-14 12:44:35 -0600

    Moved car brand logos to a dedicated `car-logos` directory within the `components` folder, following the colocation principle. Optimized the generation of cloned logos and adjusted the logic for setting CSS variables and improved overall code structure in the hero component

---
+++ app/(home)/components/logo-slider.tsx
@@ -0,0 +1,137 @@
+'use client';
+
+import { useEffect, useLayoutEffect, useRef, useState } from 'react';
+import { VolvoIcon } from './assets/car-logos/volvo';
+import { SuvaruIcon } from './assets/car-logos/suvaru';
+import { JeepIcon } from './assets/car-logos/jeep';
+import { PorscheIcon } from './assets/car-logos/porsche';
+import { VolkswagenIcon } from './assets/car-logos/volkswagen';
+import { ToyotaIcon } from './assets/car-logos/toyota';
+import { BMWIcon } from './assets/car-logos/bmw';
+import { HondaIcon } from './assets/car-logos/honda';
+import { AudiIcon } from './assets/car-logos/audi';
+import { MiniIcon } from './assets/car-logos/mini';
+import { FordIcon } from './assets/car-logos/ford';
+import { TeslaIcon } from './assets/car-logos/tesla';
+import { NissanIcon } from './assets/car-logos/nissan';
+import { KiaIcon } from './assets/car-logos/kia';
+import { HyundaiIcon } from './assets/car-logos/hyundai';
+import { MercedesBenzIcon } from './assets/car-logos/mercedes-benz';
+import { cn, setCSSVariable } from '@/app/lib/utils';
+
+type LogoData = {
+  id: string;
+  icon: JSX.Element;
+};
+
+const logoIcons = {
+  kia: <KiaIcon style={{ height: '11px', flexShrink: 0 }} />,
+  suvaru: <SuvaruIcon style={{ height: '17px', flexShrink: 0 }} />,
+  mini: <MiniIcon style={{ height: '28px', flexShrink: 0 }} />,
+  hyundai: <HyundaiIcon style={{ height: '13px', flexShrink: 0 }} />,
+  mercedesBenz: <MercedesBenzIcon style={{ height: '28px', flexShrink: 0 }} />,
+  toyota: <ToyotaIcon style={{ height: '19px', flexShrink: 0 }} />,
+  bmw: <BMWIcon style={{ height: '32px', flexShrink: 0 }} />,
+  honda: <HondaIcon style={{ height: '13px', flexShrink: 0 }} />,
+  audi: <AudiIcon style={{ height: '24px', flexShrink: 0 }} />,
+  volvo: <VolvoIcon style={{ height: '12px', flexShrink: 0 }} />,
+  volkswagen: <VolkswagenIcon style={{ height: '27px', flexShrink: 0 }} />,
+  porsche: <PorscheIcon style={{ height: '9px', flexShrink: 0 }} />,
+  nissan: <NissanIcon style={{ height: '30px', flexShrink: 0 }} />,
+  tesla: <TeslaIcon style={{ height: '10px', flexShrink: 0 }} />,
+  jeep: <JeepIcon style={{ height: '20px', flexShrink: 0 }} />,
+  ford: <FordIcon style={{ height: '34px', flexShrink: 0 }} />,
+};
+
+const initialLogos: LogoData[] = Object.entries(logoIcons).map(
+  ([id, icon]) => ({ id, icon }),
+);
+
+export function LogoSlider() {
+  const LOGO_WIDTH = '9.5rem';
+  const TOTAL_SETS_TO_CLONE = 2;
+
+  const logosListRef = useRef<HTMLUListElement>(null);
+  const [isSliderInitialized, setIsSliderInitialized] = useState(false);
+  const [clonedLogos, setClonedLogos] = useState<LogoData[]>([]);
+
+  useEffect(() => {
+    const logoList = logosListRef.current;
+
+    if (logoList) {
+      const totalLogos = initialLogos.length;
+      setCSSVariable(
+        '--slider-total-sets',
+        (TOTAL_SETS_TO_CLONE + 1).toString(),
+      );
+      setCSSVariable('--slider-total-logos', totalLogos.toString());
+      setCSSVariable('--slider-logo-width', LOGO_WIDTH);
+      setCSSVariable(
+        '--slider-total-logo-width',
+        `calc(var(--slider-total-logos) * var(--slider-logo-width) * (${TOTAL_SETS_TO_CLONE} + 1))`,
+      );
+
+      setIsSliderInitialized(true);
+    }
+  }, []);
+
+  useLayoutEffect(() => {
+    if (isSliderInitialized) {
+      setClonedLogos(generateClonedLogos(initialLogos, TOTAL_SETS_TO_CLONE));
+    }
+  }, [isSliderInitialized]);
+
+  const generateClonedLogos = (
+    logos: LogoData[],
+    totalSets: number,
+  ): LogoData[] => {
+    return Array.from({ length: totalSets }).flatMap((_, set) =>
+      logos.map(({ id, icon }) => ({
+        id: `logo-clone-${set + 1}-${id}`,
+        icon,
+      })),
+    );
+  };
+
+  return (
+    <div
+      className={cn(
+        'transition-opacity',
+        clonedLogos.length > 0 ? 'opacity-70' : 'opacity-0',
+      )}
+    >
+      <ul
+        ref={logosListRef}
+        className="flex w-[var(--slider-total-logo-width)] animate-slider items-center opacity-70 grayscale"
+      >
+        <InitialLogos />
+        {clonedLogos.map(({ id, icon }) => (
+          <li
+            key={id}
+            id={id}
+            className="mx-5 inline-flex w-[var(--slider-logo-width)] items-center justify-center"
+            aria-hidden="true"
+          >
+            {icon}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function InitialLogos() {
+  return (
+    <>
+      {initialLogos.map(({ id, icon }) => (
+        <li
+          key={id}
+          id={id}
+          className="mx-5 inline-flex w-[var(--slider-logo-width)] items-center justify-center"
+        >
+          {icon}
+        </li>
+      ))}
+    </>
+  );
+}
--- app/(home)/sections/car-search-hero.tsx
+++ app/(home)/sections/car-search-hero.tsx
@@ -3,37 +3,49 @@ import { getLocations } from '@/db/queries';
 import { CircleCheckIcon } from '@/app/components/icons/circle-check';
 import { SearchFormSkeleton } from '@/app/components/skeletons';
 import { SearchForm } from '@/app/components/search-form';
-import { LogoSlider } from '@/app/components/logo-slider';
+import { LogoSlider } from '../components/logo-slider';
 
 export async function CarSearchHero() {
   const locations = await getLocations();
 
   return (
-    <section className="bg-neutral-50 pt-12">
-      <h1 className="text-center text-3xl font-extrabold">Find your car</h1>
-      <div className="mx-auto mt-6 grid max-w-4xl grid-cols-1 items-center justify-center gap-4 md:flex md:flex-row md:gap-12">
-        <div className="flex items-center justify-center gap-1.5">
-          <CircleCheckIcon className="size-5 shrink-0 text-green-600" />
-          <span className="text-sm text-neutral-900">No hidden fees.</span>
+    <section className="bg-neutral-50">
+      <div className="pt-14 md:pt-16">
+        <div className="mx-auto flex max-w-4xl flex-col items-center justify-center">
+          <h1 className="text-center text-xl font-bold md:text-2xl">
+            Find your car
+          </h1>
+          <div className="pt-8">
+            <div className="flex flex-col items-center justify-center gap-x-9 gap-y-4 md:flex-row">
+              <span className="inline-flex items-center justify-center text-sm text-neutral-600">
+                <CircleCheckIcon className="mr-2 inline size-5 shrink-0 text-green-600" />
+                No hidden fees, ever
+              </span>
+              <span className="inline-flex items-center justify-center text-sm text-neutral-600">
+                <CircleCheckIcon className="mr-2 inline size-5 shrink-0 text-green-600" />
+                Unlimited mileage included
+              </span>
+              <span className="inline-flex items-center justify-center text-sm text-neutral-600">
+                <CircleCheckIcon className="mr-2 inline size-5 shrink-0 text-green-600" />
+                24/7 roadside assistance
+              </span>
+            </div>
+          </div>
+          <div className="hidden md:block">
+            <div className="pt-5">
+              <Suspense fallback={<SearchFormSkeleton />}>
+                <SearchForm locations={locations} />
+              </Suspense>
+            </div>
+          </div>
+          <div className="overflow-x-hidden">
+            <div className="pt-28">
+              <div className="relative flex h-24 min-h-24 w-screen items-center overflow-hidden whitespace-nowrap before:absolute before:left-0 before:top-0 before:z-10 before:h-full before:w-40 before:bg-gradient-to-r before:from-neutral-50 before:content-[''] after:absolute after:right-0 after:top-0 after:z-10 after:h-full after:w-40 after:bg-gradient-to-l after:from-neutral-50 after:content-[''] md:before:w-64 md:after:w-64">
+                <LogoSlider />
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="flex items-center justify-center gap-1.5">
-          <CircleCheckIcon className="size-5 shrink-0 text-green-600" />
-          <span className="text-sm text-neutral-900">Transparent pricing.</span>
-        </div>
-        <div className="flex items-center justify-center gap-1.5">
-          <CircleCheckIcon className="size-5 shrink-0 text-green-600" />
-          <span className="text-sm text-neutral-900">
-            Flexible cancellations.
-          </span>
-        </div>
-      </div>
-      <div className="mt-5 hidden md:block">
-        <Suspense fallback={<SearchFormSkeleton />}>
-          <SearchForm locations={locations} />
-        </Suspense>
-      </div>
-      <div className="mt-14 overflow-x-hidden">
-        <LogoSlider />
       </div>
     </section>
   );
