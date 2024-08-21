fdbf7519eb99a49900b3cc5296ed49c6418cee68
eduamdev <35645733+eduamdev@users.noreply.github.com>
2024-08-20 21:47:42 -0600

    fix: header overlap

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -9,7 +9,7 @@ import { CallToAction } from './sections/call-to-action';
 export default function HomePage() {
   return (
     <>
-      <div className="sticky top-0 z-30 bg-[hsla(0,0%,100%,.8)] shadow-[inset_0_-1px_0_0_#eaeaea] backdrop-blur-[5px] backdrop-saturate-[1.8]">
+      <div className="sticky top-0 z-40 bg-[hsla(0,0%,100%,.8)] shadow-[inset_0_-1px_0_0_#eaeaea] backdrop-blur-[5px] backdrop-saturate-[1.8]">
         <div className="mx-auto h-[var(--site-header-height)] w-full max-w-none px-5 sm:max-w-[90%] sm:px-0 2xl:max-w-8xl">
           <SiteHeader />
         </div>
